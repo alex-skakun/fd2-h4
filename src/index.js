f051284711c715fd687f7a6873dd63c79725bcf6
Buten13alex <Butan2018@mail.ru>
2023-02-15 23:57:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,64 +41,64 @@ this.array = [];
     else {
         return this.array;
 }
-}
+}}
 
 
 
 
 
 
-// создаём функции-фалидаторы
-const required = (value) => {
-    return Boolean(value) ? null : {required: true};
-};
-const minLength = (minLength) => {
-    // функция-фалидатор создаётся другой функцией
-    return (value) => {
-        return String(value).length >= minLength ? null : {minLength: true};
-    };
-};
-const maxLength = (maxLength) => {
-    return (value) => {
-        return String(value).length <= maxLength ? null : {maxLength: true};
-    };
-};
-const min = (min) => {
-    return (value) => {
-        return value >= min ? null : {min: true};
-    };
-};
-const max = (max) => {
-    return (value) => {
-        return value <= max ? null : {max: true};
-    };
-};
+// // создаём функции-фалидаторы
+// const required = (value) => {
+//     return Boolean(value) ? null : {required: true};
+// };
+// const minLength = (minLength) => {
+//     // функция-фалидатор создаётся другой функцией
+//     return (value) => {
+//         return String(value).length >= minLength ? null : {minLength: true};
+//     };
+// };
+// const maxLength = (maxLength) => {
+//     return (value) => {
+//         return String(value).length <= maxLength ? null : {maxLength: true};
+//     };
+// };
+// const min = (min) => {
+//     return (value) => {
+//         return value >= min ? null : {min: true};
+//     };
+// };
+// const max = (max) => {
+//     return (value) => {
+//         return value <= max ? null : {max: true};
+//     };
+// };
 
-// создаём экземпляр валидатора с конфигурацией по умолчанию (не передаём второй аргумент). 
-const validator = new Validator([
-    required,
-    minLength(5),
-    maxLength(25),
-]);
+// // создаём экземпляр валидатора с конфигурацией по умолчанию (не передаём второй аргумент). 
+// const validator = new Validator([
+//     required,
+//     minLength(5),
+//     maxLength(25),
+// ]);
 
-validator.validate('test'); // вернёт {minLength: true}
-validator.validate(''); // вернёт {required: true}
-validator.validate('successful'); // вернёт null
+// validator.validate('test'); // вернёт {minLength: true}
+// validator.validate(''); // вернёт {required: true}
+// validator.validate('successful'); // вернёт null
 
-validator.disable(); // выключит валидатор
-validator.validate('test'); // вернёт null, валидатор выключен, а значит значение всегда валидно
-validator.toggle(); // снова включит валидатор, т.к. в данный момент он выключен
-validator.validate('test'); // вернёт {minLength: true}
-validator.toggle(true); // оставит валидатор включённым, т.к. передано конкретное состояние
+// validator.disable(); // выключит валидатор
+// validator.validate('test'); // вернёт null, валидатор выключен, а значит значение всегда валидно
+// validator.toggle(); // снова включит валидатор, т.к. в данный момент он выключен
+// validator.validate('test'); // вернёт {minLength: true}
+// validator.toggle(true); // оставит валидатор включённым, т.к. передано конкретное состояние
 
-// создаём экземпляр валидатора с режимом 'mutli'. 
-const multiValidator = new Validator([
-    required,
-    minLength(5),
-    maxLength(25),
-], {mode: 'multi'});
+// // создаём экземпляр валидатора с режимом 'mutli'. 
+// const multiValidator = new Validator([
+//     required,
+//     minLength(5),
+//     maxLength(25),
+// ], {mode: 'multi'});
 
-multiValidator.validate(''); // вернёт {required: true, minLength: true} (две ошибки сразу)
+// multiValidator.validate(''); // вернёт {required: true, minLength: true} (две ошибки сразу)
 
 
 
